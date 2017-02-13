07037c79d2a7208ef9e6e9362eb9837bc4364737
Moch Fauzi <mister.oz17@gmail.com>
2017-02-13 18:04:46 +0700

    Update simpan.php

--- tugas2/simpan.php
+++ tugas2/simpan.php
@@ -1,3 +1,42 @@
+<?php
+include_once 'koneksi.php';
+if(isset($_GET['edit_id']))
+{
+ $sql_query="SELECT * FROM mahasiswa WHERE no=".$_GET['edit_id'];
+ $result_set=mysql_query($sql_query);
+ $fetched_row=mysql_fetch_array($result_set);
+}
+if(isset($_POST['btn-edit']))
+{
+ $nama = $_POST['nama'];
+ $nim = $_POST['nim'];
+ $alamat = $_POST['alamat'];
+ 
+ $sql_query = "UPDATE mahasiswa SET nama='$nama',nim='$nim',alamat='$alamat' WHERE no=".$_GET['edit_id'];
+
+ if(mysql_query($sql_query))
+ {
+  ?>
+  <script type="text/javascript">
+  alert('Data berhasil di edit');
+  window.location.href='home.php';
+  </script>
+  <?php
+ }
+ else
+ {
+  ?>
+  <script type="text/javascript">
+  alert('Terjadi error saat edit data');
+  </script>
+  <?php
+ }
+}
+if(isset($_POST['btn-batal']))
+{
+ header("Location: home.php");
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -54,24 +93,24 @@
 			</nav>
 			<!-- Start Bagian Article -->
 			<article>
-				<form action="" method="">
+				<form method="POST">
 					<table border="1" width="60%" align="center">
 						<tr>
 							<th><align ="center" colspan="2">Simpan data</th>
 						</tr>
 						<tr>
 							<td bgcolor="#00FFFF"><strong>Nama</strong></td>
-						  <td><Input style="text" name="nama" size="80"></td>
+						  <td><Input style="text" name="nama" size="80" value="<?php echo $fetched_row['nama']; ?>" /></td>
 						</tr>
 						<tr>
 							<td bgcolor="#00FFFF"><strong>NIM</strong></td>
-						  <td><input type=" text" name="Nim" size="80"></td>
+						  <td><input type=" text" name="nim" size="80" value="<?php echo $fetched_row['nim']; ?>"></td>
 						</tr>
 						<tr>
 							<td bgcolor="#00FFFF"><strong>Alamat</strong></td>
-						  <td><input type=" text" name="alamat" size="80"></td>
+						  <td><input type=" text" name="alamat" size="80"value="<?php echo $fetched_row['alamat']; ?>"></td>
 						</tr>
-							<td colspan="2" align="right"><input type="button" value="Simpan" /> <input type="button" value="Batal" /></td>
+							<td colspan="2" align="right"><input type="submit" name="btn-edit" value="Simpan" /> <input type="submit" value="Batal" name="btn-batal" /></td>
 						</tr>
 					</table>
 				</form>
