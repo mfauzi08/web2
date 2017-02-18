c5e1dbe473e529f252f331152dfc4ce24b95efa6
Moch Fauzi <mister.oz17@gmail.com>
2017-02-18 19:44:00 +0700

    Update index.php

--- tugas2/index.php
+++ tugas2/index.php
@@ -1,50 +1,31 @@
 <?php
 	session_start();
-	include 'koneksi.php';
-	if (isset($_POST['btn-login'])){
-		$username = $_POST['username'];
-		$password = sh1($_POST['password']);
-		
-		$sql_query = "SELECT * FROM login WHERE username = '$username' and password = '$password'";
-		
-		if(mysql_query($sql_query)){
-			$num_rows = mysql_num_rows(mysql_query($sql_query));
-			if($num_rows == 1){
-				$_session['username'] = $username;
-				?>
-		<script type="text/javascript">
-			alerts('Anda Berhasil Login');
-			window.location.href='home.php';
-		</script>
-		<?php
-			}
-		else{
-			?>
-		<script type="text/javascript">
-			alerts('Username anda Password Tidak cocok');
-			window.location.href='index.php';
-		</script>
-		<?php
-		}
-		} else {
-			?>
-		<script type="text/javascript">
-			alerts('Terjadi Error');
-			window.location.href='index.php';
-		</script>
-		<?php
-		}
-	}
-	if(isset($_POST['batal'])){
-		?>
-		<script type="text/javascript">
-			window.location.href='index.php';
-		</script>
-		<?php
-	}
+	if(isset($_POST['login'])){
+		include "koneksi.php";
+		$query=mysql_query("SELECT * FROM login WHERE username='$_POST[username]' and password='$_POST[password]'");
+		$row=mysql_num_rows($query);
+
+		if($row==1){
+			$_SESSION['username']=$_POST['username'];
+			$_SESSION['password']=($_POST['password']);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<script type="text/javascript">
+	alert('Anda Berhasil Login');
+	document.location="home.php";
+</script>
+<?php
+	} else {
+?>
+<script type="text/javascript">
+	alert('Anda Gagal Login, Silakan Cek User/Password Anda atau Silakan Daftar Terlebih Dahulu');
+	document.location="index.php";
+</script>
+<?php
+}
+}
+?>
+<!DOCTYPE html>
+<html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 		<title>Operasi CRUD</title>
@@ -81,6 +62,7 @@
 			}
 		</style>
 	</head>
+	
 	<body>
 		<div class="Container">
 			<!-- Start Bagian Header -->
@@ -100,23 +82,12 @@
 			<!-- End Bagian SideNav -->
 			<!-- Start Bagian Article -->
 			<article>
-				<form method="POST">
-					<table border="1" width="75%" align="center">
-						<tr>
-							<th colspan="2" align="center">Login <a href="#">Daftar</a></th>
-							</tr>
-						<tr>
-							<td>Username</td>
-							<td><input type="text" name="username" size="80"/></td>
-						</tr>
-						<tr>
-							<td>Password</td>
-							<td><input type="password" name="password" size="80"/></td>
-						</tr>
-						<tr>
-							<td colspan="2" align="right"><input type="submit" value="Simpan" name="btn-login"/> <input type="submit" value="Batal" name="btn-batal"/></td>
-						</tr>
-					</table>
+				<form action="" method="post">
+					<label>UserName :</label>
+					<input id="name" name="username" placeholder="username" type="text">
+					<label>Password :</label>
+					<input id="password" name="password" placeholder="**********" type="password">
+					<input name="login" type="submit" value=" Login ">
 				</form>
 			</article>
 			<!-- End Bagian Article -->
@@ -124,5 +95,5 @@
 			<footer>@Mohamad Fauzi - 14.111.146</footer>
 			<!-- End Bagian Footer -->
 		</div>
-	</body>
+	</body>		
 </html>
