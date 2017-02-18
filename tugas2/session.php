c72f0bcb2877572bb2515dc341462a8b384d3503
Moch Fauzi <mister.oz17@gmail.com>
2017-02-18 19:41:45 +0700

    Update session.php

--- tugas2/session.php
+++ tugas2/session.php
@@ -1,16 +1,31 @@
 <?php
-	include 'koneksi.php';
 	session_start();
-	
-	$user_check = $_session['username'];
-	
-	$mysql_guery = "SELECT * FROM login WHERE username = '$user_check'";
-	
-	$row = mysql_fetch_array(mysql_query)
-	
-	$login_session_name = $row['nama'];
-	
-	if(isset($_session['username'])){
-		header("location:index.php");
-		}
+	include "koneksi.php";
+
+	if(isset($_POST['login'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		$sql = mysql_query("SELECT * FROM login WHERE username='$username' && password='$password'");
+		
+		$num = mysql_num_rows($sql);
+
+		if($num==1) {	
+			$_SESSION['username'] = $username;
+			$_SESSION['password'] = $password;
+?>
+<script language="JavaScript">
+	alert('Username atau password Anda salah');
+	document.location='home.php';
+</script>
+<?php
+} 
+else {
+?>
+<script language="JavaScript">
+	alert('Username atau password Anda salah');
+	document.location='index.php';</script>
+<?php
+}
+}
 ?>
